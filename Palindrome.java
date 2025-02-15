c36c13001c56449b3152fd88f14c9e52bf8990a7
Trần Anh Trí <105036360+tatgnusmas2004@users.noreply.github.com>
2025-02-15 12:54:09 +0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,5 @@
+/* Given an integer x, return true if x is a palindrome, and false otherwise.*/
+
 public class Palindrome {
   public boolean isPalindrome(int x) {
     if (x < 0 || (x % 10 == 0) && x != 0) {
